209761dc04f461d10fb95954c0bbc9a32e8b0784
Fedor Serkov <serkovfb@gmail.com>
2020-04-25 00:17:39 +1000

    Use functions.

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from mutagen.mp3 import MP3
 import pygame
 
 DIR = '/home/fedor/Music/'
-filename = os.path.join(DIR, 'Zivert - Life.mp3')
-print(MP3(filename).info.length)
 
-pygame.init()
-pygame.mixer.music.load(filename)
-pygame.mixer.music.play()
-sleep(10)
+
+def get_length(filename):
+    return MP3(filename).info.length
+
+
+def play(filename):
+    pygame.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    sleep(10)
+
+
+if __name__ == '__main__':
+    _filename = os.path.join(DIR, os.listdir(DIR)[0])
+    print(get_length(_filename))
+    play(_filename)
