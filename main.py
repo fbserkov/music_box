74a359117c2b6145e2d733ec8a58bc138a484b17
Fedor Serkov <serkovfb@gmail.com>
2020-04-24 23:54:09 +1000

    Reading length of music file. Add requirements.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
 from time import sleep
+
+from mutagen.mp3 import MP3
 import pygame
 
+DIR = '/home/fedor/Music/'
+filename = os.path.join(DIR, 'Zivert - Life.mp3')
+print(MP3(filename).info.length)
+
 pygame.init()
-pygame.mixer_music.load('/home/fedor/Music/Zivert - Life.mp3')
-pygame.mixer_music.play()
+pygame.mixer.music.load(filename)
+pygame.mixer.music.play()
 sleep(10)
