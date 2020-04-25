87577182ffaea0723e8f16ed34a729636972ed0c
Fedor Serkov <serkovfb@gmail.com>
2020-04-25 19:14:27 +1000

    Read play list from separated py-file.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import sleep
 from mutagen.mp3 import MP3
 import pygame
 
-DIR = '/home/fedor/Music/'
+from playlist import DIR, my_list
 
 
 def get_length(filename):
@@ -15,10 +15,12 @@ def play(filename):
     pygame.init()
     pygame.mixer.music.load(filename)
     pygame.mixer.music.play()
-    sleep(10)
+    sleep(get_length(_filename))
 
 
 if __name__ == '__main__':
-    _filename = os.path.join(DIR, os.listdir(DIR)[0])
-    print(get_length(_filename))
-    play(_filename)
+    _filename = os.path.join(DIR, my_list[0]['filename'])
+    while True:
+        sleep(1)
+        if False:  # Если пора запускать
+            play(_filename)
---
+++ playlist.py
@@ -0,0 +1,4 @@
+my_list = (
+    dict(hours=19, minutes=00, filename='Zivert - Life.mp3'),
+)
+DIR = '/home/fedor/Music/'
