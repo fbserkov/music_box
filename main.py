4c4197e62674767906e0aa8bd57e665a9e78f279
Fedor Serkov <serkovfb@gmail.com>
2020-04-27 11:49:48 +1000

    First satisfactory result.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from datetime import datetime
 import os
+from importlib import reload
 from time import sleep
 
 from mutagen.mp3 import MP3
 import pygame
 
-from playlist import DIR, my_list
+import playlist
+
+
+def check():
+    reload(playlist)
+    now = datetime.now()
+    current = 60 * now.hour + now.minute
+    for _dict in playlist.LIST:
+        expected = 60 * _dict['hour'] + _dict['minute']
+        print(current, expected)
+        if current == expected:
+            play(os.path.join(playlist.DIR, _dict['filename']))
 
 
 def get_length(filename):
@@ -15,12 +28,10 @@ def play(filename):
     pygame.init()
     pygame.mixer.music.load(filename)
     pygame.mixer.music.play()
-    sleep(get_length(_filename))
+    sleep(get_length(filename))
 
 
 if __name__ == '__main__':
-    _filename = os.path.join(DIR, my_list[0]['filename'])
     while True:
-        sleep(1)
-        if False:  # Если пора запускать
-            play(_filename)
+        check()
+        sleep(10)
--- playlist.py
+++ playlist.py
@@ -1,4 +1,6 @@
-my_list = (
-    dict(hours=19, minutes=00, filename='Zivert - Life.mp3'),
-)
 DIR = '/home/fedor/Music/'
+LIST = (
+    dict(hour=11, minute=44, filename='candyman_19_cue_2b_piano.mp3'),
+    dict(hour=11, minute=46, filename='candyman_19_cue_2b_piano.mp3'),
+    dict(hour=11, minute=48, filename='Zivert - Life.mp3'),
+)
