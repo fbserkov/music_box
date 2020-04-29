91f96d54f13920d5ddd7886b4b27c86106a18e77
Fedor Serkov <serkovfb@gmail.com>
2020-04-29 12:14:50 +1000

    Исправление, так как Music._music_dir инициализируется после создания экземпляров.

--- music.py
+++ music.py
@@ -14,17 +14,18 @@ class Music:
         time = time + ':00' if len(time) == 5 else time
         self._time = datetime.strptime(
             f'{date.today()} {time}', '%Y-%m-%d  %H:%M:%S')
-        self.filename = os.path.join(Music._music_dir, mp3 + '.mp3')  # TODO Проверка существования файла
+        self._mp3 = mp3 + '.mp3'  # TODO Проверка существования файла
         self.volume = volume
 
     def __str__(self):
-        return f'{self._time} - {self.filename}'
+        return f'{self._time} - {self._mp3}'
 
     def _play(self):
-        pygame.mixer.music.load(self.filename)
+        filename = os.path.join(Music._music_dir, self._mp3)
+        pygame.mixer.music.load(filename)
         pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play()
-        sleep(MP3(self.filename).info.length)
+        sleep(MP3(filename).info.length)
 
     def __call__(self):
         if str(self._time) == str(datetime.now()).split('.')[0]:  # TODO
