826e980d6b88d4a1d00afc9c6e0807e42805d2bd
Fedor Serkov <serkovfb@gmail.com>
2020-04-29 12:03:46 +1000

    Сохранение в экземпляре Music времени в виде объекта datetime.

--- music.py
+++ music.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 import os
 from time import sleep
 
@@ -11,26 +11,25 @@ class Music:
     _music_dir = ''
 
     def __init__(self, time, mp3, volume=1.0):
-        if len(time) == 5:
-            time += ':00'
-        self.time = time  # TODO Проверка корректности значения времени
-        self.mp3 = mp3  # TODO Проверка существования файла
+        time = time + ':00' if len(time) == 5 else time
+        self._time = datetime.strptime(
+            f'{date.today()} {time}', '%Y-%m-%d  %H:%M:%S')
+        self.filename = os.path.join(Music._music_dir, mp3 + '.mp3')  # TODO Проверка существования файла
         self.volume = volume
 
     def __str__(self):
-        return f'{self.time} - {self.mp3}'
-
-    def __call__(self):
-        if self.time == datetime.now().strftime('%H:%M:%S'):
-            print(self)
-            self._play()
+        return f'{self._time} - {self.filename}'
 
     def _play(self):
-        filename = os.path.join(Music._music_dir, self.mp3 + '.mp3')
-        pygame.mixer.music.load(filename)
+        pygame.mixer.music.load(self.filename)
         pygame.mixer.music.set_volume(self.volume)
         pygame.mixer.music.play()
-        sleep(MP3(filename).info.length)
+        sleep(MP3(self.filename).info.length)
+
+    def __call__(self):
+        if str(self._time) == str(datetime.now()).split('.')[0]:  # TODO
+            print(self)
+            self._play()
 
 
 if __name__ == '__main__':  # tests
